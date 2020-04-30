5aec2470e7bd7d9830f96b81fba4bd6d29c6aa60
yeosingng <yeosingng@gmail.com>
2020-04-29 22:55:04 -0400

    Add basic layout for pages

---
+++ components/Layout.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import styled from 'styled-components'
+
+export default function Layout(props) {
+  return (
+    <div>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Sprite Friend</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div id='main'>
+        {props.children}
+      </div>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+
+const Main = styled.main`
+  height: 100%;
+`
+
+const LayoutContainer = styled.div`
+  height: 100vh;
+`
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
-import Head from 'next/head'
+import Layout from '../components/Layout'
+import styled from 'styled-components'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Sprite Friend</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-
-
-
-
-      </main>
-    </div>
+    <Layout>
+      <Container>
+        <PetBox>
+          <Pet src='/sprites/spritey.png' />
+        </PetBox>
+      </Container>
+    </Layout>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  height: 100vh;
+`
+
+const PetBox = styled.div`
+  padding: 20px;
+  height: fit-content;
+`
+
+const Pet = styled.img`
+  height: 200px;
+  width: 200px;
+`
+
+const Something = styled.div`
+  color: red;
+`
